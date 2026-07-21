db78a320445cb44411c47eb181f97f8b6796bb62
vr051688-collab <vr051688@gmail.com>
2026-07-21 09:02:17 +0530

    Update User.js

--- app/backend/models/User.js
+++ app/backend/models/User.js
@@ -5,7 +5,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true, lowercase: true },
   password: { type: String, required: true }, // bcrypt hash
   role: { type: String, enum: ['admin', 'employee'], default: 'employee' },
-  employeeId: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee', default: null }
+  employeeId: { type: mongoose.Schema.Types.ObjectId, ref: 'Employee', default: null },
   resetOtp: { type: String, default: null },
   resetOtpExpires: { type: Date, default: null },
   }, { timestamps: true });
